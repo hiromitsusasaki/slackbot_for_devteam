34f6aa4c0f64ef81daee01a514fc2d6c22a47b07
hiromitsusasaki <hiromitsu.sasaki@gmail.com>
2023-01-16 22:17:07 +0900

    wip: add remind related github repositries

--- manifest.ts
+++ manifest.ts
@@ -2,6 +2,7 @@ import { Manifest } from "deno-slack-sdk/mod.ts";
 import SampleWorkflow from "./workflows/sample_workflow.ts";
 import SampleObjectDatastore from "./datastores/sample_datastore.ts";
 
+
 /**
  * The app manifest contains the app's configuration. This
  * file defines attributes like app name and description.
@@ -9,16 +10,14 @@ import SampleObjectDatastore from "./datastores/sample_datastore.ts";
  */
 export default Manifest({
   name: "slackbot_for_devteam",
-  description: "A template for building Slack apps with Deno",
+  description: "Useful chatbot for devteam on Slack",
   icon: "assets/default_new_app_icon.png",
-  workflows: [SampleWorkflow],
+  workflows: [SampleWorkflow, RemindGithubPrReview],
   outgoingDomains: [],
   datastores: [SampleObjectDatastore],
   botScopes: [
     "commands",
     "chat:write",
     "chat:write.public",
-    "datastore:read",
-    "datastore:write",
   ],
 });
